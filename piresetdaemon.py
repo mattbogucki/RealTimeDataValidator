aad3ec2205cc4caa4b1ebc5302ac8dc7159f2480
Matthew.Bogucki <matt.bogucki.829@gmail.com>
2022-09-22 17:10:29 -0700

    Perfecto - Release it
    Column sizing is good, and got rid of the superfluous sheet

--- piresetdaemon.py
+++ piresetdaemon.py
@@ -1,10 +1,15 @@
 import argparse
+import datetime
 import os
 import socket
 import sys
 
 import PIconnect
 import openpyxl
+from openpyxl.worksheet.dimensions import ColumnDimension, DimensionHolder
+from openpyxl.utils import get_column_letter
+
+BIG_NUMBER = 99999999999999
 
 
 def main():
@@ -41,6 +46,18 @@ def get_pi_server_name() -> str:
         return socket.gethostname()
     except:
         return socket.gethostname()
+
+
+# TODO Could make this even more fitting to the data size, but good enough for now
+def size_columns_to_be_readable(ws):
+    dim_holder = DimensionHolder(worksheet=ws)
+
+    for col in range(ws.min_column, ws.max_column + 1):
+        column_letter = get_column_letter(col)
+        column_width = 70 if column_letter == "A" else 25
+        dim_holder[column_letter] = ColumnDimension(ws, min=col, max=col, width=column_width)
+
+    ws.column_dimensions = dim_holder
 
 
 class PointsMaster(object):
@@ -135,15 +152,15 @@ class PointsMaster(object):
 
                 # Check to ensure all data falls within min/max from attachment 3
                 if max_recorded_value > egu_max:
-                    max_violations.append((point, egu_max, max_recorded_value))
+                    max_violations.append((point, egu_max, max_recorded_value, device_type, source_device))
                 if min_recorded_value < egu_min:
-                    min_violations.append((point, egu_min, min_recorded_value))
+                    min_violations.append((point, egu_min, min_recorded_value, device_type, source_device))
 
                 # Should be a value at least every 5 minutes at a bare minimum
                 if recorded_values_count < 288:
-                    frequency_of_update_violations.append((point, recorded_values_count))
+                    frequency_of_update_violations.append((point, recorded_values_count, device_type, source_device))
 
-                smallest_delta = 9999999999999999
+                smallest_delta = BIG_NUMBER
 
                 # Should record deltas at a minimum of 0.1
                 for i in range(recorded_values_count):
@@ -153,11 +170,11 @@ class PointsMaster(object):
                     current_value = data[i+1]
 
                     delta = abs(current_value - previous_value)
-                    if delta < smallest_delta:
+                    if delta < smallest_delta and delta != 0:  # 0 doesnt count
                         smallest_delta = delta
 
                 if smallest_delta >= 1:
-                    granularity_violations.append((point, smallest_delta))
+                    granularity_violations.append((point, smallest_delta, device_type, source_device))
 
         print("Total Points Audited: {}".format(points_audited))
 
@@ -169,7 +186,7 @@ class PointsMaster(object):
 
         excel_file_output = openpyxl.Workbook()
         excel_file_output.title = "Real-Time-Data-Audit"
-        excel_file_output.create_sheet("Min Violations")
+        excel_file_output.active.title = "Min Violations"
         excel_file_output.create_sheet("Max Violations")
         excel_file_output.create_sheet("Update Freq Violations")
         excel_file_output.create_sheet("Granularity Violations")
@@ -178,35 +195,59 @@ class PointsMaster(object):
         min_violations_ws.cell(row=1, column=1).value = "Point Name"
         min_violations_ws.cell(row=1, column=2).value = "A11 EGU Min"
         min_violations_ws.cell(row=1, column=3).value = "Recorded Min"
+        min_violations_ws.cell(row=1, column=4).value = "Device Type"
+        min_violations_ws.cell(row=1, column=5).value = "Source Device"
         for i, min_violation in enumerate(min_violations, start=2):  # Start after header row openpyxl is 1 based
             min_violations_ws.cell(row=i, column=1).value = min_violation[0]
             min_violations_ws.cell(row=i, column=2).value = min_violation[1]
             min_violations_ws.cell(row=i, column=3).value = min_violation[2]
+            min_violations_ws.cell(row=i, column=4).value = min_violation[3]
+            min_violations_ws.cell(row=i, column=5).value = min_violation[4]
 
         max_violations_ws = excel_file_output["Max Violations"]
         max_violations_ws.cell(row=1, column=1).value = "Point Name"
         max_violations_ws.cell(row=1, column=2).value = "A11 EGU Max"
         max_violations_ws.cell(row=1, column=3).value = "Recorded Max"
+        max_violations_ws.cell(row=1, column=4).value = "Device Type"
+        max_violations_ws.cell(row=1, column=5).value = "Source Device"
         for j, max_violation in enumerate(max_violations, start=2):
             max_violations_ws.cell(row=j, column=1).value = max_violation[0]
             max_violations_ws.cell(row=j, column=2).value = max_violation[1]
             max_violations_ws.cell(row=j, column=3).value = max_violation[2]
+            max_violations_ws.cell(row=j, column=4).value = max_violation[3]
+            max_violations_ws.cell(row=j, column=5).value = max_violation[4]
 
         freq_violations_ws = excel_file_output["Update Freq Violations"]
         freq_violations_ws.cell(row=1, column=1).value = "Point Name"
         freq_violations_ws.cell(row=1, column=2).value = "Updates in last 24 hours"
+        freq_violations_ws.cell(row=1, column=3).value = "Device Type"
+        freq_violations_ws.cell(row=1, column=4).value = "Source Device"
         for k, frequency_of_update_violation in enumerate(frequency_of_update_violations, start=2):
             freq_violations_ws.cell(row=k, column=1).value = frequency_of_update_violation[0]
             freq_violations_ws.cell(row=k, column=2).value = frequency_of_update_violation[1]
+            freq_violations_ws.cell(row=k, column=3).value = frequency_of_update_violation[2]
+            freq_violations_ws.cell(row=k, column=4).value = frequency_of_update_violation[3]
 
         granularity_violations_ws = excel_file_output["Granularity Violations"]
         granularity_violations_ws.cell(row=1, column=1).value = "Point Name"
         granularity_violations_ws.cell(row=1, column=2).value = "Smallest Granularity Change in Last 24 hours"
+        granularity_violations_ws.cell(row=1, column=3).value = "Device Type"
+        granularity_violations_ws.cell(row=1, column=4).value = "Source Device"
         for m, granularity_violation in enumerate(granularity_violations, start=2):
             granularity_violations_ws.cell(row=m, column=1).value = granularity_violation[0]
-            granularity_violations_ws.cell(row=m, column=2).value = granularity_violation[1]
+            if granularity_violation[1] == BIG_NUMBER:
+                granularity_violations_ws.cell(row=m, column=2).value = "Not Updating"
+            else:
+                granularity_violations_ws.cell(row=m, column=2).value = granularity_violation[1]
+            granularity_violations_ws.cell(row=m, column=3).value = granularity_violation[2]
+            granularity_violations_ws.cell(row=m, column=4).value = granularity_violation[3]
 
-        excel_file_output.save("{}-Real-Time-Data-Audit.xlsx".format(get_pi_server_name()))
+        for sheet in excel_file_output.sheetnames:
+            ws = excel_file_output[sheet]
+            size_columns_to_be_readable(ws)
+
+        now = datetime.datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
+        excel_file_output.save("{}-Real-Time-Data-Audit_{}.xlsx".format(get_pi_server_name(), now))
 
 
 if __name__ == "__main__":
